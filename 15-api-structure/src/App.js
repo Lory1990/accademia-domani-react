cdb8acbca905a50c56d225d015baa765f83962fe
Lorenzo De Francesco <lory1990@gmail.com>
2021-06-23 19:26:08 +0200

    Added one repo

--- 15-api-structure/src/App.js
+++ 15-api-structure/src/App.js
@@ -9,16 +9,25 @@ function App() {
 
   const [cocktail, setCocktail] = useState()
 
+  const [loading, setLoading] = useState()
+  const [error, setError] = useState()
+
   const fetchRandomCocktail = async () =>{
     try{
+      setLoading(true)
+      setError(undefined)
       const result = await axios({
         method: "GET",
         url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
       })
 
+      console.log("Cocktail result", result.data.drinks[0])
       setCocktail(result.data.drinks[0])      
+      return result
     }catch(e){
-      console.error(e)
+      setError(e.message)
+    }finally{
+      setLoading(false)
     }
   }
 
@@ -30,7 +39,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>The cocktail is: {cocktail?.strDrink}</div>
+        <div>
+          {loading ? 
+            <code>We are loadig the cocktail</code>
+            :
+            <>
+              {error ? 
+                <div>Error is: {error}</div>  
+                :
+                <>
+                  {cocktail ?
+                    <div>The cocktail is: {cocktail?.strDrink}</div>
+                    :
+                    <div>There are no cocktails</div>
+                  }
+                </>
+              }
+            </>
+          }          
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
